7ba4bb5c60c8abafcb605ec5c33d0f721d605a5e
ezequielspaixao-max <ezequielspaixao@gmail.com>
2026-09-05 07:38:03 -0300

    Define versão KP Electrical Tools BETA v1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.nortech.capacitores"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "10.0-beta"
+        versionCode = 11
+        versionName = "1.0-beta"
     }
 
     buildFeatures { compose = true }
